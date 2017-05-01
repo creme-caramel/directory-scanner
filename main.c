1e0582a63183b205e1a5e32dac7288ce6da8359b
mimiko <lykrysh@email.com>
2017-05-01 10:58:04 -0700

    Retrieve correct arr

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 #include "db.h"
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #define BUFF 256
 
 int main(int argc, char **argv)
 {
-	char *str;
-	size_t str_i;
+	//char *str;
+	//size_t str_i;
 	FILE *f;
 	int rc;
 	sqlite3 *db;
-	cols vals;
+	//cols vals;
 	unsigned char c;
 
 	// Reopen db
@@ -23,66 +22,71 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	// groupsizes.txt
-	f = fopen(argv[2], "r");
-	if(f == NULL) {
-		perror("can't open file\n");
-		return -1;
-	}
-	fseek(f, 0, SEEK_SET);
-	int skip_punct = -6;
-	int on = 0;
-	if(!(str = malloc(1))) {
-		fprintf(stderr, "oops can't malloc str\n");
-		return -1;
-	}
-	vals.number_of_groups_analyzed = 0;		
-	do {
-		c = fgetc(f);
-		if(feof(f)) {
-			break;
-		}
-		if(c == ',') {
-			skip_punct++;
-			on++;
-			str_i = 0;
-		}
-		else if(c == '\n') {
-			on--;
-			str[str_i] = '\0';
-			vals.number_of_groups_analyzed += atoi(&str[1]);
-			//printf("%s\n", &str[1]);
-		}
-		if(skip_punct > 0 && on) {
-			str[str_i] = c;
-			str_i++;
-			if(!(str = realloc(str, 1))) {
-				fprintf(stderr, "oops can't realloc str\n");
-				return -1;
-			}	
-		}			
-	} while(1);
-	free(str);
-	fclose(f);
-
 	// output.txt
-	f = fopen(argv[3], "r"); 
+	f = fopen(argv[2], "r"); 
 	if(f == NULL) {
 		perror("can't open file\n");
 		return -1;
 	}
 	fseek(f, 0, SEEK_SET);
+
+	int on = 0;
+	char header[7];
 	do {
 		c = fgetc(f);
 		if(feof(f)) {
 			break;
 		}
-		//printf("%c", c);
+
+		if(c == '.') {
+			do {
+				c = fgetc(f);
+			} while (c != ',');
+			c = fgetc(f);
+			size_t idx = 0;
+			do {
+				header[idx++] = c;
+				c = fgetc(f);
+			} while (c != ',');
+			header[idx] = '\0';
+			//printf("%s\n", header);
+			on++;
+		}
+
+		if(on) {
+			char *arr[3];
+			int i;
+			for(i = 0; i < 3; i++) {
+				size_t idx = 0;
+				arr[i] = malloc(1);
+				do {
+					arr[i][idx] = c;
+					idx++;
+					arr[i] = realloc(arr[i], idx+1);
+					c = fgetc(f);
+					if(feof(f)) {
+						break;
+					}
+				} while (c != ',' && c != '\n');
+				arr[i][idx] = '\0';
+				if(arr[i][0] == ',')
+					memmove(arr[i], arr[i]+1, strlen(arr[i]+1) + 1);
+			}
+			printf("%s\t%s %s %s\n", header, arr[0], arr[1], arr[2]);
+			free(arr[0]);
+			free(arr[1]);
+			free(arr[2]);
+			//printf("%c", c);
+			if(c == '\n') {
+				on--;
+			}
+		}
 	} while(1);
+	printf("\n");
 /*
 	str = malloc(BUFF);
 	strcpy(str, "update my_table set number_of_groups_analyzed = ");
-	char temp[4];
+	char temp[3];
 	sprintf(temp, "%u", vals.number_of_groups_analyzed);
 	strcat(str, temp);
 	strcat(str, " where ID=");
--- table_details.h
+++ table_details.h
@@ -13,7 +13,6 @@
 typedef struct cols {
 	unsigned int filtered;
 	unsigned int number_of_groups_analyzed;
-	unsigned int number_of_raw_groups;
 	unsigned int number_of_contaminations;
 	unsigned int a_to__;
 	unsigned int a_to_c;
